9dc554051faa45b97c276c3cbfaf0eb500d758ec
Zach Poba-Nzaou <168301948+plectus24@users.noreply.github.com>
2024-05-17 02:20:35 -0400

    Initialize canvas
    1st version of canvas with drawing functionality

---
+++ script.js
@@ -0,0 +1,53 @@
+console.log("hi");
+// create 16 divs by default
+function createDivs(size = 16) {
+  const container = document.querySelector(".container");
+  for (let i = 0; i < size; i++) {
+    const div = document.createElement("div");
+    div.className = "canvas";
+    container.appendChild(div);
+  }
+}
+//clear canvas
+function clearCanvas() {
+  const tile = [...document.querySelectorAll(".canvas")];
+  tile.forEach((item) => {
+    item.remove();
+  });
+}
+
+//create canvas
+function createCanvas() {
+  const canvasCreate = document.querySelector(".create-canvas");
+  canvasCreate.addEventListener("click", (e) => {
+    let size = -1;
+    while (size < 0 || size > 100) {
+      // Don't change anything if promt cancelled
+      size = prompt("Enter a size");
+      if (size === null) {
+        size = document.querySelectorAll(".canvas");
+        console.log(size);
+      }
+      console.log("hjkiwebs");
+    }
+    // clearCanvas();
+    createDivs(size);
+    canvasHover();
+  });
+}
+
+// draw function
+function canvasHover() {
+  const tile = [...document.querySelectorAll(".canvas")];
+
+  // leave trail on hover
+  tile.forEach((item) => {
+    item.addEventListener("mouseover", (e) => {
+      item.style.backgroundColor = "grey";
+    });
+  });
+}
+
+createDivs();
+canvasHover();
+createCanvas();
