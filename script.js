5be1d387bf3bacfde7c71b21277974933424ecae
Zach Poba-Nzaou <168301948+plectus24@users.noreply.github.com>
2024-05-20 18:56:39 -0400

    Fix grid logic + styling
    Fixed logic for grid drawing and added proper styling to reflect changes

--- script.js
+++ script.js
@@ -2,15 +2,35 @@ console.log("hi");
 // create 16 divs by default
 function createDivs(size = 16) {
   const container = document.querySelector(".container");
+  const canvasRow = document.createElement("div");
+  // const row = document.createElement("div");
   for (let i = 0; i < size; i++) {
-    const div = document.createElement("div");
-    div.className = "canvas";
-    container.appendChild(div);
+    const row = document.createElement("div");
+    row.className = "canvas-row";
+    container.appendChild(row);
   }
+  const allRows = [...document.querySelectorAll(".canvas-row")];
+  allRows.forEach((item) => {
+    for (let i = 0; i < size; i++) {
+      const div = document.createElement("div");
+      div.className = "canvas";
+      div.style.width = (window.innerWidth * 2) / 3 / size + "px";
+      div.style.height = (window.innerHeight * 2) / 3 / size + "px";
+      item.appendChild(div);
+    }
+  });
+
+  // console.log(canvasRow);
+
+  // for (let j = 0; j < size; j++) {
+  //   // const canvasRow = document.createElement("div");
+  //   canvasRow.className = "canvas-row";
+  //   container.appendChild(canvasRow);
+  // }
 }
 //clear canvas
 function clearCanvas() {
-  const tile = [...document.querySelectorAll(".canvas")];
+  const tile = [...document.querySelectorAll(".canvas-row")];
   tile.forEach((item) => {
     item.remove();
   });
@@ -22,7 +42,7 @@ function createCanvas() {
   canvasCreate.addEventListener("click", (e) => {
     let curSize = [...document.querySelectorAll(".canvas")].length;
     let size = -1;
-    while (size < 0 || size > 100) {
+    while (size <= 0 || size > 100) {
       // Don't change anything if promt cancelled
       size = prompt("Enter a size");
       if (size === null) {
